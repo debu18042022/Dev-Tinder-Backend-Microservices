35d4db9a740cb34e87065e17a802eb55398e751b
debu18042022 <sujeetsahu322@gmail.com>
2025-07-28 08:04:35 +0530

    playing with multiple request handler.

--- src/app.js
+++ src/app.js
@@ -2,15 +2,36 @@ const express = require("express");
 
 const app = express();
 
-/**here :means it a dynamic route */
-app.get("/user/:userId/:Fname/:Lname", (req, res) => {
-  console.log("req.query", req.query);
-  console.log("req.params", req.params);
-  res.send({
-    firstName: "Debu",
-    lastName: "Sahu",
-  });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("route hander 1");
+    // res.send("User route handler 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("route hander 2");
+    // res.send("User route handler 2");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("route hander 3");
+      // res.send("User route handler 3");
+      next();
+    },
+    (req, res, next) => {
+      console.log("route hander 4");
+      // res.send("User route handler 4");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("route hander 5");
+    res.send("User route handler 5");
+    next();
+  }
+);
 
 app.listen(7777, () => {
   console.log("server is running on port 7777");
